6db5360fd0d5aeb5aacc537d68f5ce3745ca4a73
Lazinelia <lazinelia@gmail.com>
2020-10-23 12:17:37 +0900

    feat: change table structure

--- initdb/1_table.sql
+++ initdb/1_table.sql
@@ -11,9 +11,13 @@ CREATE TABLE IF NOT EXISTS user_data (
     id BIGSERIAL PRIMARY KEY,
     guild_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
+    level BIGINT NOT NULL,
     own_exp BIGINT NOT NULL,
     next_exp BIGINT NOT NULL,
     total_exp BIGINT NOT NULL,
+    last_message_timestamp TIMESTAMP,
     UNIQUE(guild_id, user_id),
     CHECK(own_exp < next_exp)
 );
+
+CREATE INDEX exp_rank ON user_data (total_exp DESC);
